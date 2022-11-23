b5a84284e19476fe720b72e6bd033561eda363e6
Syed Muhammad Ali Naqvi <sn07590@st.habib.edu.pk>
2022-11-23 05:49:06 +0500

    bgscreen

--- HUMania/game.cpp
+++ HUMania/game.cpp
@@ -1,59 +1,61 @@
 #include "game.hpp"
 #include "LandSeige.hpp"
 #include "drawing.hpp"
+#include <SDL_ttf.h>
 
-SDL_Renderer* Drawing::gRenderer = NULL;
-SDL_Texture* Drawing::assets = NULL;
+#include <Windows.h>
+
+SDL_Renderer *Drawing::gRenderer = NULL;
+SDL_Texture *Drawing::assets = NULL;
 
 bool Game::init()
 {
-	//Initialization flag
+	// Initialization flag
 	bool success = true;
 
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	// Initialize SDL
+	TTF_Init();
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
 		success = false;
 	}
 	else
 	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		// Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
 		{
-			printf( "Warning: Linear texture filtering not enabled!" );
+			printf("Warning: Linear texture filtering not enabled!");
 		}
 
-		//Create window
-		gWindow = SDL_CreateWindow( "HU Mania", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
+		// Create window
+		gWindow = SDL_CreateWindow("HU Mania", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
 		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
 			success = false;
 		}
 		else
 		{
-			//Create renderer for window
-			Drawing::gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-			if( Drawing::gRenderer == NULL )
+			// Create renderer for window
+			Drawing::gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (Drawing::gRenderer == NULL)
 			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
 				success = false;
 			}
 			else
 			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( Drawing::gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				// Initialize renderer color
+				SDL_SetRenderDrawColor(Drawing::gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-				//Initialize PNG loading
+				// Initialize PNG loading
 				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				if (!(IMG_Init(imgFlags) & imgFlags))
 				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
 					success = false;
 				}
-
 			}
 		}
 	}
@@ -63,98 +65,99 @@ bool Game::init()
 
 bool Game::loadMedia()
 {
-	//Loading success flag
+	// Loading success flag
 	bool success = true;
-	
+
 	Drawing::assets = loadTexture("assets.png");
-    gTexture = loadTexture("sdlDrawing.png");
-	if(Drawing::assets==NULL || gTexture==NULL)
-    {
-        printf("Unable to run due to error: %s\n",SDL_GetError());
-        success =false;
-    }
+	gTexture = loadTexture("abnew.png");
+	if (Drawing::assets == NULL || gTexture == NULL)
+	{
+		printf("Unable to run due to error: %s\n", SDL_GetError());
+		success = false;
+	}
 	return success;
 }
 
 void Game::close()
 {
-	//Free loaded images
+	// Free loaded images
+	TTF_Quit();
 	SDL_DestroyTexture(Drawing::assets);
-	Drawing::assets=NULL;
+	Drawing::assets = NULL;
 	SDL_DestroyTexture(gTexture);
-	
-	//Destroy window
-	SDL_DestroyRenderer( Drawing::gRenderer );
-	SDL_DestroyWindow( gWindow );
+
+	// Destroy window
+	SDL_DestroyRenderer(Drawing::gRenderer);
+	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
 	Drawing::gRenderer = NULL;
-	//Quit SDL subsystems
+	// Quit SDL subsystems
 	IMG_Quit();
 	SDL_Quit();
 }
 
-SDL_Texture* Game::loadTexture( std::string path )
+SDL_Texture *Game::loadTexture(std::string path)
 {
-	//The final texture
-	SDL_Texture* newTexture = NULL;
+	// The final texture
+	SDL_Texture *newTexture = NULL;
 
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-	if( loadedSurface == NULL )
+	// Load image at specified path
+	SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+	if (loadedSurface == NULL)
 	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
 	}
 	else
 	{
-		//Create texture from surface pixels
-        newTexture = SDL_CreateTextureFromSurface( Drawing::gRenderer, loadedSurface );
-		if( newTexture == NULL )
+		// Create texture from surface pixels
+		newTexture = SDL_CreateTextureFromSurface(Drawing::gRenderer, loadedSurface);
+		if (newTexture == NULL)
 		{
-			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
 		}
 
-		//Get rid of old loaded surface
-		SDL_FreeSurface( loadedSurface );
+		// Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
 	}
 
 	return newTexture;
 }
-void Game::run( )
+void Game::run()
 {
 	bool quit = false;
 	SDL_Event e;
 
 	LandSeige landSeige;
 
-	while( !quit )
+	while (!quit)
 	{
-		//Handle events on queue
-		while( SDL_PollEvent( &e ) != 0 )
+		// Handle events on queue
+		while (SDL_PollEvent(&e) != 0)
 		{
-			//User requests quit
-			if( e.type == SDL_QUIT )
+			// User requests quit
+			if (e.type == SDL_QUIT)
 			{
 				quit = true;
 			}
 
-			if(e.type == SDL_MOUSEBUTTONDOWN){
-			//this is a good location to add pigeon in linked list.
+			if (e.type == SDL_MOUSEBUTTONDOWN)
+			{
+				// this is a good location to add pigeon in linked list.
 				int xMouse, yMouse;
-				SDL_GetMouseState(&xMouse,&yMouse);
+				SDL_GetMouseState(&xMouse, &yMouse);
 				landSeige.createUserEquipment(xMouse, yMouse);
 			}
 		}
 
-		SDL_RenderClear(Drawing::gRenderer); //removes everything from renderer
-		SDL_RenderCopy(Drawing::gRenderer, gTexture, NULL, NULL);//Draws background to renderer
+		SDL_RenderClear(Drawing::gRenderer);					  // removes everything from renderer
+		SDL_RenderCopy(Drawing::gRenderer, gTexture, NULL, NULL); // Draws background to renderer
 		//***********************draw the objects here********************
 
 		landSeige.drawObjects();
 
 		//****************************************************************
-    	SDL_RenderPresent(Drawing::gRenderer); //displays the updated renderer
+		SDL_RenderPresent(Drawing::gRenderer); // displays the updated renderer
 
-	    SDL_Delay(100);	//causes sdl engine to delay for specified miliseconds
+		SDL_Delay(100); // causes sdl engine to delay for specified miliseconds
 	}
-			
 }
